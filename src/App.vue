2f07942eaccdbbed8596567371b10c168c4f7e9a
matteofab10 <matteofabio10@gmail.com>
2021-11-26 14:22:37 +0100

    creazione repo con bootstrap e font

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="container">
+      <h1>ciao bootstrap</h1>
+    </div>
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+@import '~bootstrap/scss/bootstrap.scss';
+@import '~@fontsource/alegreya-sans/index.css';
+@import '~@fontsource/alegreya-sans/700.css';
+
+body {
+  font-family: "Alegreya Sans"; 
 }
+
+
 </style>
