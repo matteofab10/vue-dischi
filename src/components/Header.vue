6a0711c3adb6ff85e1b7d68a3dca304ba89300e3
matteofab10 <matteofabio10@gmail.com>
2021-11-29 16:04:26 +0100

    creo input select

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,21 @@
     <div class="logo-spotify">  
       <img src="../assets/spotify-logo.png" alt="logo-spotify">
     </div>
+    <div>
+      <Selected />
+    </div>
   </header>
 </template>
 
 <script>
+
+import Selected from './Selected.vue';
+
 export default {
   name: "Header",
+  components: {
+    Selected,
+  },
 }
 </script>
 
@@ -18,13 +27,16 @@ export default {
   header {
     height: 60px;
     background-color: #2e3a46;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     .logo-spotify {
       width: 50px;
       height: 50px;
     }
     img {
       width: 100%;
-      margin: 5px 0 5px 15px;
+      margin-left: 15px;
       cursor: pointer;
     }
   
---
+++ src/components/Selected.vue
@@ -0,0 +1,26 @@
+<template>
+  <select class="selector">
+    <option>metal</option>
+    <option>jazz</option>
+    <option>pop</option>
+  </select>  
+</template>
+
+<script>
+
+export default {
+  name: "Selected",
+}
+</script>
+
+<style lang="scss">
+  @import "../assets/style/vars.scss";
+
+  .selector{
+  height: 30px;
+  width: 100px;
+  padding: 5px;
+  margin-right: 10px;
+}
+
+</style>
